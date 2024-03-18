7dc0bd02297d334ff0666e93ec06f5317f58d613
Michael <enjoyweaver@gmail.com>
2024-03-18 18:00:26 -0500

    grammar

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,11 +11,11 @@
       text-align: center;
     "
   >
-    Planet Keeper is an app focused on tracking the population status of our
-    pollinators around the globe. Data loss, data categoriazation, and data
-    integrity are major concerns when collecting, storing, and analyzing data
-    for our pollinators around the globe. Arweave is a global computer that
-    allows us to store data permanently while giving the participants the
-    ability to contribute to that data.
+    Planet Keeper is focused on tracking the population status of our
+    pollinators around the globe and contributing to their preservation. Data
+    loss, data categoriazation, and data integrity are major concerns when
+    collecting, storing, and analyzing data for our pollinators around the
+    globe. Arweave is a global computer that allows us to store data permanently
+    while giving the participants the ability to contribute to that data.
   </div>
 </template>
