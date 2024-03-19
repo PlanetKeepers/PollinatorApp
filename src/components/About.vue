5a08a2a3c7c127e26c6c3b259d5f2daf51bf40d3
Michael <enjoyweaver@gmail.com>
2024-03-18 21:49:29 -0500

    about

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,29 +12,36 @@
     <p style="font-size: xxx-large">About</p>
     <p>
       Pollinator is focused on tracking the population status of our pollinators
-      around the globe and contributing to their preservation. Data loss, data
-      categoriazation, and data integrity are major concerns when collecting,
-      storing, and analyzing data for our pollinators around the globe. Arweave
-      is a global computer that allows us to store data permanently while giving
-      the participants the ability to contribute to that data.
+      around the globe and contributing to their preservation. Data loss and
+      data integrity are major concerns when collecting, storing, and analyzing
+      information for our pollinators. Arweave is a global computer that allows
+      us to store data permanently while giving the participants the ability to
+      contribute to that data.
     </p>
     <p>
-      Of the 4,000+ species of bees in the United States, we only know the
-      population status of roughly 7% of those bees, meaning that we do not
-      understand the population trends of 93% of the bees in the United States.
-      These percentages are likely similar around the world and not just in the
-      United States.
+      Of the 4,000+ species of pollinators in the United States, we only know
+      the population status of roughly 7% of those pollinators, meaning that we
+      do not understand the population trends of 93% in the United States. These
+      percentages are likely similar around the world.
     </p>
     <p>
       Our mission is to preserve the pollinators through data and education.
-      First, we need data to understand the population status's of our
-      pollinators, and second we need education to expand the role, purpose, and
-      scope of the pollinator data.
+      First, we need data to understand the population statuses, and second we
+      need education to expand the role, purpose, and scope of the pollinator
+      data.
     </p>
     <p>
-      Our first step is to onboard individuals to Arweave to allow them to
-      contribute to the data. This includes farmers, hobbyists, educators,
-      researchers, and creditable organizations.
+      Our first step is to onboard individuals to Arweave so that they can
+      contribute to the newly created global pollinator database. This includes
+      farmers, hobbyists, educators, researchers, credible organizations, and
+      you.
+    </p>
+    <p>
+      There are two key aspects to the pollinator data though, the first is the
+      trends of the populations, and the second if the food sources for the
+      pollinators. Some pollinators are only specific to certain areas of the
+      globe, and if their foods are disappearing, then the pollinators will as
+      well.
     </p>
   </div>
 </template>
